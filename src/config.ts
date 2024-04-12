ad92adc810fd927d41326a2b82026eb61daaa9dc
Joel De Conto <joel_d.c@hotmail.com>
2024-04-12 19:35:23 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 import path from "path";
 
-export const CURRENT_CLUB_FILE_PATH = process.env.VERCEL_ENV ? '/tmp/currentClub.json' : path.join(process.cwd(), 'tmp/currentClub.json');
+export const CURRENT_CLUB_FILE_PATH = process.env.VERCEL_ENV ? 'tmp/currentClub.json' : path.join(process.cwd(), 'tmp/currentClub.json');
